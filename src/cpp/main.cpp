d5dd334b6872fdbc548a0da43c38b1183231a4fb
Hrishikesh VK <kv_hrishikesh@yahoo.com>
2015-03-09 23:17:55 +0530

    simple application entry main, defines the general flow at startup

---
+++ src/cpp/main.cpp
@@ -0,0 +1,26 @@
+int main(){
+
+	/*
+ *	read problem id
+ *	Load id based .so
+ *		Instantiate Solution object
+ *		Instantiate Test Case Iterator
+ *		Test Case : { Input Object, Max Time, Max memory, Reference Output Object}
+ *
+ *	Instantiate Invoker & Verifier
+ *
+ *	create timed thread ; Thr func { Use Invoker to call Solution function, and use verifier to return result, write to a file}
+ *	main thread : do timed sleep, kill thread if required, gather result and return by writing result to file.
+ *
+ *
+ *	Input & output object : Int, String, (Array Of Int/String/array & int size), ListNode, TreeNode - Can Invoker and Verifier be COMMON ?
+ *
+ *	For verifier, write templatized Compare functions for each known object types
+ *
+ *
+ *
+ * 	*/
+
+	return 0;
+}
+
